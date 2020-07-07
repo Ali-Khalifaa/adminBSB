f60c8fa286ae513ca89a3236e5072c48960316c6
AliKhalifa <ali689.kh@gmail.com>
2020-07-08 00:24:15 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TypographyComponent } from './typography/typography.component';
+import { HelperClassesComponent } from './helper-classes/helper-classes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"typography",component:TypographyComponent},
+  {path:"helper-classes",component:HelperClassesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TypographyComponent } from './typography/typography.component';
+import { HelperClassesComponent } from './helper-classes/helper-classes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TypographyComponent,
+    HelperClassesComponent
   ],
   imports: [
     BrowserModule,
